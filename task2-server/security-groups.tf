046ef551968ae977064a8d2623c8285b07ff2b2b
elijaharley <heyelijaharley@gmail.com>
2024-11-11 11:59:42 +0000

    add resource examples task 2

---
+++ task2-server/security-groups.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "allow_ssh" {
+    # TO BE IMPLEMENTED
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ingress" {
+    # TO BE IMPLEMENTED
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_ssh_egress" {
+    # TO BE IMPLEMENTED
+}
