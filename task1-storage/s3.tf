4c1839ede4fb18c612ea8612cb875529216c1865
nc-harrison-r <harrison.royds@northcoders.com>
2025-05-01 16:10:16 +0100

    all

--- task1-storage/s3.tf
+++ task1-storage/s3.tf
@@ -1,7 +1,10 @@
 resource "aws_s3_bucket" "demo_bucket" {
-  # TO BE IMPLEMENTED
+  bucket_prefix = "harrisons-demo-bucket-"
 }
 
 resource "aws_s3_object" "demo_object" {
   # TO BE IMPLEMENTED
+  bucket = aws_s3_bucket.demo_bucket.id
+  key    = "text_file"
+  source = "test_file.txt"
 }
